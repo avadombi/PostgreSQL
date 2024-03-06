65942e552f45acbb1e20683065ed189d258d4af9
avadombi <vp.adombi@gmail.com>
2024-03-06 18:21:23 -0500

    PostgreSQL with VS Code

---
+++ 002-RedoTutorial/scripts/002-create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS companies(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    country VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees(
+    id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    company_id BIGINT NOT NULL,
+
+    -- foreign key and its behavior on delete and update
+    FOREIGN KEY (company_id) REFERENCES companies (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
