b2c7bfcb581a432d37550accdfe637ad218c38b4
avadombi <vp.adombi@gmail.com>
2024-03-07 09:47:16 -0500

    Insert data manually, delete data

---
+++ 002-RedoTutorial/scripts/004-insert_manually.sql
@@ -0,0 +1,10 @@
+-- insert only one row
+INSERT INTO companies(id, name, country)
+VALUES (1001, 'Naruto Company', 'Naruto Home');
+
+-- insert multiple rows
+INSERT INTO companies(id, name, country)
+VALUES
+    (1002, 'Spiderman inc.', 'Spiderman land'),
+    (1003, 'DBZ email', 'Dangerous Dragons');
+
--- 002-RedoTutorial/scripts/005-select_data.sql
+++ 002-RedoTutorial/scripts/005-select_data.sql
@@ -1,1 +1,10 @@
 -- SELECT * FROM companies LIMIT 5;
+SELECT * FROM companies WHERE name LIKE 'Naruto%' OR name LIKE 'Spiderman%' OR name LIKE 'DBZ%';
+
+-- Or
+SELECT * FROM companies WHERE id IN (1001, 1002, 1003);
+
+-- Or
+SELECT * FROM companies WHERE id BETWEEN 1001 AND 1003;
+
+
